747544bec17428488e9af897a988ef5d18c5b500
Amit Kumar <amitkumar.developer1@gmail.com>
2021-02-05 23:13:15 +0530

    Added readme and server port

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 7000;
 
 const userRouter = require('./routes/user');
 
